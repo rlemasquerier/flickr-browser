df9a2c69235ef760274aa9338eacc9371a0e9c99
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2020-06-07 18:04:04 +0200

    handle errors in GetRawData

---
+++ app/src/main/java/com/datmos/flickrbrowser/GetRawData.kt
@@ -0,0 +1,56 @@
+package com.datmos.flickrbrowser
+
+import android.os.AsyncTask
+import android.util.Log
+import java.io.IOException
+import java.lang.Exception
+import java.net.MalformedURLException
+import java.net.URL
+
+private const val TAG = "GetRawData"
+
+enum class DownloadStatus {
+    OK, IDLE, NOT_INITIALISED, FAILED_OR_EMPTY, PERMISSIONS_ERROR, ERROR
+}
+
+class GetRawData : AsyncTask<String, Void, String>() {
+    private var downloadStatus = DownloadStatus.IDLE
+
+    override fun doInBackground(vararg params: String?): String {
+        if (params[0] == null) {
+            downloadStatus = DownloadStatus.NOT_INITIALISED
+            return "No URL specified"
+        }
+
+        try {
+            downloadStatus = DownloadStatus.OK
+            return URL(params[0]).readText()
+        } catch (error: Exception) {
+            val errorMessage = when (error) {
+                is MalformedURLException -> {
+                    downloadStatus = DownloadStatus.NOT_INITIALISED
+                    "doInBackground: Invalid URL ${error.message}"
+                }
+                is IOException -> {
+                    downloadStatus = DownloadStatus.FAILED_OR_EMPTY
+                    "doInBackground: IO Exception reading data: ${error.message}"
+                }
+                is SecurityException -> {
+                    downloadStatus = DownloadStatus.PERMISSIONS_ERROR
+                    "doInBackground: SecurityException, needs permissions? ${error.message}"
+                }
+                else -> {
+                    downloadStatus = DownloadStatus.ERROR
+                    "doInBackground: Unknown error: ${error.message}"
+                }
+            }
+            Log.d(TAG, errorMessage)
+
+            return errorMessage
+        }
+    }
+
+    override fun onPostExecute(result: String?) {
+        super.onPostExecute(result)
+    }
+}
